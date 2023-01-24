60bd164890b621c0f69e1c27949575aa55e592b0
Carlos Lostao <hastro1990@gmail.com>
2023-01-24 17:46:38 +0100

    fix: missing keystore folderpath

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 if [ -n "$SEED" ]; then
+
   ./target/release/frontier-template-node key insert --base-path /tmp/node \
+  --keystore-path  /tmp/node/chains/alphanet/keystore \
   --scheme Sr25519 \
   --suri "$SEED" \
   --key-type aura
 
   ./target/release/frontier-template-node key insert \
+  --keystore-path  /tmp/node/chains/alphanet/keystore \
   --base-path /tmp/node \
   --scheme Ed25519 \
   --suri "$SEED" \
@@ -18,5 +21,5 @@ if [ -n "$BOOTNODE" ]; then
   ./target/release/frontier-template-node --base-path /tmp/node --validator --bootnodes "$BOOTNODE" --chain alphanet
 else
   echo "Starting node without bootnode"
-  ./target/release/frontier-template-node --base-path /tmp/node --validator --chain alphanet
+  ./target/release/frontier-template-node --base-path /tmp/node --validator --chain alphanet 
 fi
