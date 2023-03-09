8d3503b6b9124ad4d1ecfb180354da21135ec457
Carlos Lostao <hastro1990@gmail.com>
2023-03-09 15:27:15 +0100

    fix(testing): pallet supported tokens manager

--- pallets/token-fee-controller/supported-tokens-manager/src/lib.rs
+++ pallets/token-fee-controller/supported-tokens-manager/src/lib.rs
@@ -14,6 +14,8 @@ pub mod pallet {
 
 	use super::*;
 
+	use frame_support::pallet_prelude::*;
+
 	use frame_support::{
 		storage::types::{OptionQuery, StorageMap, StorageValue, ValueQuery},
 		Blake2_128Concat,
