d2a62949d84addeb91eb34a09612222632eee774
GabrielMartinezRodriguez <gabrielmartinezrdrgz@gmail.com>
2023-03-10 16:39:28 +0100

    chore: changed copyright messages

--- test-utils/stability-runtime/src/genesismap.rs
+++ test-utils/stability-runtime/src/genesismap.rs
@@ -1,6 +1,6 @@
-// This file is part of Substrate.
+// This file is part of Stability.
 
-// Copyright (C) 2017-2022 Parity Technologies (UK) Ltd.
+// Copyright 2023 Stability Solutions.
 // SPDX-License-Identifier: Apache-2.0
 
 // Licensed under the Apache License, Version 2.0 (the "License");
