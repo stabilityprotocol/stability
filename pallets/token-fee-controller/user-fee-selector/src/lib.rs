163185eb82fbddd786bb752e3eaf9bf7557dbf4f
Carlos Lostao <hastro1990@gmail.com>
2023-05-03 11:01:34 +0200

    refactor: user fee token selector

--- pallets/token-fee-controller/user-fee-selector/src/lib.rs
+++ pallets/token-fee-controller/user-fee-selector/src/lib.rs
@@ -13,16 +13,13 @@ pub mod pallet {
 
 	use super::*;
 
-	use frame_support::traits::Hooks;
 	use frame_support::{
-		storage::types::{OptionQuery, StorageMap, StorageValue},
+		storage::types::{OptionQuery, StorageMap},
 		Blake2_128Concat,
 	};
-	use frame_system::pallet_prelude::BlockNumberFor;
 	use pallet_erc20_manager::ERC20Manager;
 	use pallet_supported_tokens_manager::SupportedTokensManager;
 	use sp_core::H160;
-	use sp_std::vec::Vec;
 
 	#[pallet::pallet]
 	#[pallet::without_storage_info]
@@ -51,12 +48,6 @@ pub mod pallet {
 		OptionQuery,
 	>;
 
-	// todo: remove this logic
-	#[pallet::storage]
-	#[pallet::getter(fn pending_token_updates)]
-	pub type PendingTokenUpdatesStorage<T: Config> =
-		StorageValue<_, Vec<(H160, H160)>, OptionQuery>;
-
 	impl<T: Config> UserFeeTokenController for Pallet<T> {
 		type Error = Error<T>;
 
@@ -73,26 +64,13 @@ pub mod pallet {
 			if !T::SupportedTokensManager::is_supported_token(token) {
 				return Err(Error::<T>::UnsupportedToken);
 			}
-			let mut pending_updates = PendingTokenUpdatesStorage::<T>::get().unwrap_or_default();
-			pending_updates.push((account, token));
-			PendingTokenUpdatesStorage::<T>::put(pending_updates);
+			FeeTokenStorage::<T>::insert(account, token);
 			Ok(())
 		}
 
 		fn balance_of(account: H160) -> U256 {
 			let token = Self::get_user_fee_token(account);
 			T::ERC20Manager::balance_of(token, account)
-		}
-	}
-
-	#[pallet::hooks]
-	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
-		fn on_finalize(_n: T::BlockNumber) {
-			let pending_updates = PendingTokenUpdatesStorage::<T>::get().unwrap_or_default();
-			for (account, token) in pending_updates {
-				FeeTokenStorage::<T>::insert(account, token);
-			}
-			PendingTokenUpdatesStorage::<T>::kill();
 		}
 	}
 }
