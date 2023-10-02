ad7714536589e6bc3ddb77ea4c14da230ab1c7e0
GabrielMartinezRodriguez <gabrielmartinezrdrgz@gmail.com>
2023-10-02 10:29:32 +0200

    chore: added zero gas tx pool to docker

--- docker/client/entrypoint.sh
+++ docker/client/entrypoint.sh
@@ -46,10 +46,12 @@ if [ -n "$NODE_KEY" ]; then
   START_COMMAND="$START_COMMAND --node-key $NODE_KEY"
 fi
 
-if [ "$DEFAULT_BOOTNODE" = "yes" ]; then
-  START_COMMAND="$START_COMMAND --bootnodes /ip4/3.21.88.38/tcp/30333/p2p/12D3KooWPaen1igo2WYUFCt3EAg4AWjWoMYgmr4tCa2Yb1WfgoDB"
-elif [ -n "$BOOTNODES" ]; then
+if [ -n "$BOOTNODES" ]; then
   START_COMMAND="$START_COMMAND --bootnodes $BOOTNODES"
+fi
+
+if [ -n "$ZERO_GAS_TX_POOL" ]; then
+  START_COMMAND="$START_COMMAND --zero-gas-tx-pool $ZERO_GAS_TX_POOL"
 fi
 
 echo "Starting $CHAIN_TARGET chain"
