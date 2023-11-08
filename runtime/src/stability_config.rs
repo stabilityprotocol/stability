566a3c731937ecc4087006eb7971b7a2f4ee8c92
clostao <49268919+clostao@users.noreply.github.com>
2023-11-08 17:03:20 +0100

    fix: default token config (#137)

--- runtime/src/stability_config.rs
+++ runtime/src/stability_config.rs
@@ -14,7 +14,7 @@ const COMPUTATION_POWER_MULTIPLIER: u64 = 6; // 6 times more computation power t
 // how much weight for normal extrinsics could be processed in a block
 pub const MAXIMUM_BLOCK_WEIGHT: Weight = Weight::from_ref_time(WEIGHT_REF_TIME_PER_MILLIS)
 	.mul(MILLISECS_PER_BLOCK)
-	.mul(COMPUTATION_POWER_MULTIPLIER) 
+	.mul(COMPUTATION_POWER_MULTIPLIER)
 	.mul(COMPUTATION_BLOCK_TIME_RATIO.0)
 	.div(COMPUTATION_BLOCK_TIME_RATIO.1) // 8_000_000_000_000
 	.set_proof_size(u64::MAX);
@@ -32,7 +32,7 @@ pub const COUNCIL_MAX_MEMBERS: u32 = 100;
 
 pub const DEFAULT_OWNER: &str = "0xaf537bd156c7E548D0BF2CD43168dABF7aF2feb5";
 
-pub const DEFAULT_FEE_TOKEN: &str = "0xDc2B93f3291030F3F7a6D9363ac37757f7AD5C43";
+pub const DEFAULT_FEE_TOKEN: &str = "0x261FB2d971eFBBFd027A9C9Cebb8548Cf7d0d2d5";
 
 /// Minimum deposit of an account to exist.
 /// Since this minimum deposit would be reduced
