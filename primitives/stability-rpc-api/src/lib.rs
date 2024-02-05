2e7fa6613e2e2b18f027f29a1bc175f547341026
Carlos Lostao <hastro1990@gmail.com>
2024-02-05 11:11:29 +0100

    feat: stability_getActiveValidatorList endpoint

--- primitives/stability-rpc-api/src/lib.rs
+++ primitives/stability-rpc-api/src/lib.rs
@@ -10,6 +10,8 @@ sp_api::decl_runtime_apis! {
 
 		fn get_validator_list() -> Vec<H160>;
 
+		fn get_active_validator_list() -> Vec<H160>;
+
 		fn convert_sponsored_transaction(transaction: fp_ethereum::Transaction, meta_trx_sponsor: H160, meta_trx_sponsor_signature: Vec<u8>) -> <Block as BlockT>::Extrinsic;
 	}
 }
