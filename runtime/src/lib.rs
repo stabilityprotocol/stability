8439467002c2efce0cf1ef154cbdc9698ba781e9
Daniel Blanco Parla <dblancoparla@gmail.com>
2025-01-03 15:18:48 -0800

    fix(runtime): add runtime versioning for improved stability tracking

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -166,6 +166,7 @@ pub mod opaque {
 	}
 }
 
+#[sp_version::runtime_version]
 pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("node-stability"),
 	impl_name: create_runtime_str!("node-stability"),
