e1205743855b4b545e96aa89687b6eb74a32a253
Daniel Blanco Parla <dblancoparla@gmail.com>
2025-02-03 12:13:57 +0100

    fix(primitives): update CustomFeeInfo to support TransactionV2 and enhance fee calculation logic

--- primitives/tools/src/custom_fee.rs
+++ primitives/tools/src/custom_fee.rs
@@ -1,4 +1,5 @@
-use fp_ethereum::{Transaction, TransactionData};
+use ethereum::TransactionV2;
+use fp_ethereum::TransactionData;
 use sp_core::U256;
 
 pub struct CustomFeeInfo {
@@ -8,8 +9,8 @@ pub struct CustomFeeInfo {
 }
 
 impl CustomFeeInfo {
-	pub fn new(base_fee: U256, transaction: &Transaction) -> Self {
-		let data: TransactionData = TransactionData::from(transaction);
+	pub fn new(base_fee: U256, transaction: &TransactionV2) -> Self {
+		let data: TransactionData = transaction.into();
 		custom_info_from_fee_params(
 			base_fee,
 			data.max_fee_per_gas.or(data.gas_price),
@@ -23,12 +24,37 @@ pub fn custom_info_from_fee_params(
 	max_fee_per_gas: Option<U256>,
 	max_priority_fee_per_gas: Option<U256>,
 ) -> CustomFeeInfo {
+	// Calculate the reduced max_fee_per_gas
+	let reduced_max_fee_per_gas = match (max_fee_per_gas, max_priority_fee_per_gas) {
+		// With tip, we include as much of the tip on top of base_fee that we can, never
+		// exceeding max_fee_per_gas
+		(Some(max_fee_per_gas), Some(max_priority_fee_per_gas)) => {
+			let actual_priority_fee_per_gas = max_fee_per_gas
+				.saturating_sub(base_fee)
+				.min(max_priority_fee_per_gas);
+
+			base_fee.saturating_add(actual_priority_fee_per_gas)
+		}
+		// Without tip, we include as much of the base_fee as we can, never exceeding
+		(Some(max_fee_per_gas), None) => {
+			if max_fee_per_gas == U256::zero() {
+				max_fee_per_gas // It's a ZGT transaction
+			} else if max_fee_per_gas < base_fee {
+				base_fee
+			} else {
+				max_fee_per_gas
+			}
+		}
+		// If there is no max_fee_per_gas, we just use the base_fee added to the max_priority_fee_per_gas
+		(None, Some(max_priority_fee_per_gas)) => max_priority_fee_per_gas.saturating_add(base_fee),
+		// If there is no max_fee_per_gas and no max_priority_fee_per_gas, we just use the base_fee
+		_ => base_fee,
+	};
+
 	CustomFeeInfo {
 		max_conversion_rate: max_priority_fee_per_gas
-			.map(|_| (max_fee_per_gas.unwrap(), 1_000_000_000.into())),
-		max_fee_per_gas: max_fee_per_gas
-			.or(max_priority_fee_per_gas.map(|e| e.saturating_add(base_fee)))
-			.unwrap_or_default(),
+			.map(|_| (reduced_max_fee_per_gas, 1_000_000_000.into())),
+		max_fee_per_gas: reduced_max_fee_per_gas,
 		max_priority_fee_per_gas,
 	}
 }
@@ -67,26 +93,34 @@ mod test {
 
 	#[test]
 	fn custom_info_from_fee_params_for_trx_v2() {
+		let base_fee = U256::from(1_000_000_000);
+		let max_fee_x_gas = U256::from(2_000_000_000);
+		let max_priority_fee_x_gas = U256::from(500_000_000);
+
 		let info = custom_info_from_fee_params(
-			U256::from(1_000_000_000),
-			Some(U256::from(2_000_000_000)),
-			Some(U256::from(500_000_000)),
+			base_fee,
+			Some(max_fee_x_gas),
+			Some(max_priority_fee_x_gas),
 		);
 
-		assert_eq!(info.max_priority_fee_per_gas, Some(U256::from(500_000_000)));
-		assert_eq!(info.max_fee_per_gas, U256::from(2_000_000_000));
+		assert_eq!(info.max_priority_fee_per_gas, Some(max_priority_fee_x_gas));
+		assert_eq!(
+			info.max_fee_per_gas,
+			base_fee.saturating_add(max_priority_fee_x_gas)
+		);
 		assert_eq!(
 			info.max_conversion_rate,
-			Some((U256::from(2_000_000_000), U256::from(1_000_000_000)))
+			Some((max_fee_x_gas, U256::from(1_000_000_000)))
 		);
 	}
 
 	#[test]
 	fn custom_info_from_fee_params_for_read_trx() {
-		let info = custom_info_from_fee_params(U256::from(1_000_000_000), None, None);
+		let base_fee = U256::from(1_000_000_000);
+		let info = custom_info_from_fee_params(base_fee, None, None);
 
 		assert_eq!(info.max_priority_fee_per_gas, None);
-		assert_eq!(info.max_fee_per_gas, U256::from(0));
+		assert_eq!(info.max_fee_per_gas, base_fee);
 		assert_eq!(info.max_conversion_rate, None);
 	}
 }
