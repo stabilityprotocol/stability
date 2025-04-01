d4e1a58c97f1fe0db5a8c32a53deae61ab857537
Daniel Blanco Parla <dblancoparla@gmail.com>
2025-04-01 21:20:27 +0200

    fix: update zero gas transaction check and adjust test cases for fee handling

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -175,7 +175,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("node-stability"),
 	impl_name: create_runtime_str!("node-stability"),
 	authoring_version: 1,
-	spec_version: 4,
+	spec_version: 5,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
